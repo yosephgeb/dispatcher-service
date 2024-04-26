155d38e2f8bf374667bccef590d2b6d1ada25d19
yosephog <thorfry@yahoo.com>
2024-04-26 14:15:15 +0200

    comment out test class for the time being

--- src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
+++ src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
@@ -1,13 +1,8 @@
 package com.polarbookshop.dispatcherservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.function.context.FunctionCatalog;
 import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
-import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
-
-import java.util.function.Function;
 
 @FunctionalSpringBootTest
 public class DispatchingFunctionsIntegrationTests {
